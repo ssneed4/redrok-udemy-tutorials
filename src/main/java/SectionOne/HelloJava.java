a3ffb6a3963ef02d08eda172ab215ac233a95b3c
ssneed4 <redrok1988@gmail.com>
2023-08-24 16:58:29 -0500

    Added SectionOne HelloJava.java file

---
+++ src/main/java/SectionOne/HelloJava.java
@@ -0,0 +1,12 @@
+package src.main.java.SectionOne;
+
+/**
+ * HelloJava
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Java World");
+    }
+    
+}
