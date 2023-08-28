bf9dd0b70c94b938db38093c11f4f81c723f2104
ssneed4 <redrok1988@gmail.com>
2023-08-28 14:50:53 -0500

    Added Variables section

--- SectionTwo/src/main/java/Variables.java
+++ SectionTwo/src/main/java/Variables.java
@@ -1,6 +1,11 @@
 package src.main.java;
 public class Variables {
     public static void main(String[] args) {
-        System.out.println();
+
+        int passengers = 5;
+        System.out.println("Number of passengers is:" + passengers);
+
+        int busTickets = passengers;
+        
     }
 }
